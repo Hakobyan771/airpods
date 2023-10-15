4bb117abc4844033edc3fbec2157994ccbd36705
Xach Hakobyan <xachhakobyan20@gmail.com>
2023-10-15 16:34:03 +0300

    js finish

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const choseColor = document.querySelectorAll('.chose__color-btn');
-const content = document.querySelectorAll('.content__item');
+const contentItem = document.querySelectorAll('.content__item');
 
 
 choseColor.forEach(function(element){
@@ -8,11 +8,22 @@ choseColor.forEach(function(element){
 
 function open(evt){
     const target = evt.currentTarget;
+    const button = target.dataset.button;
+    const contentActive = document.querySelectorAll(`.${button}`);
+
 
     choseColor.forEach(function(item){
         item.classList.remove('chose__color-btn--active')
     })
 
     target.classList.add('chose__color-btn--active')
+
+   contentItem.forEach(function(item){
+    item.classList.remove('content__item--active')
+   });
+
+   contentActive.forEach(function(item){
+    item.classList.add('content__item--active')
+   })
 
 }
